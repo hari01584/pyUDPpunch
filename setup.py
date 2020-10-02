9aec50674783a50464665fee29418164a3718446
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2020-10-02 19:20:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'pyUDPpunch',         # How you named your package folder (MyLib)
   packages = ['pyUDPpunch'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='GNU v3',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='GPLv3+',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Punching Holes Through UDP Sockets!!',   # Give a short description about your library
   author = 'Harishankar Kumar',                   # Type in your name
   author_email = 'hari01584@gmail.com',      # Type in your E-Mail
@@ -17,7 +17,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNUv3 License',   # Again, pick a license
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
